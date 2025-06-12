b314e7502eb7b8fdd989286a887e52bbc6f0e30f
Tharunreddy165 <tharungudipelly@gmail.com>
2025-06-12 14:53:16 +0530

    Add files via upload

---
+++ DataTypesEx.java
@@ -0,0 +1,37 @@
+package ajp;
+
+public class DataTypesEx
+
+{ public static void main(String args[])
+
+{
+
+//declaring variables
+
+char ch='a';
+
+boolean value=true;
+
+byte num1=100;
+
+short num2=200;
+
+int num3=1200;
+
+long num4=120000;//
+
+float num5=3.14f;
+
+double num6=321.09876;
+
+//Print values
+
+System.out.println(ch+","+value);
+
+System.out.println("Integer Type: "+num1+", "+num2+", "+num3+", "+num4);
+
+System.out.println("Floating Values: "+num5+", "+num6);
+
+}//main
+
+}//class
