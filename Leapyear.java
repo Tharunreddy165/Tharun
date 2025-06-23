53743fddeae62f7f9c8877bf91ef2df8a2e46eed
Tharunreddy165 <tharungudipelly@gmail.com>
2025-06-23 17:02:24 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,21 @@
+package ajp;
+
+import java.util.Scanner;
+
+public class Leapyear{
+	public static void main(String[] args) {
+		Scanner sc = new 
+				Scanner (System.in);
+		System.out.print("Enter year:");
+		int year = sc.nextInt();
+		if(isLeapyear(year)) {
+			System.out.println(year + "is a Leap year.");
+		}else
+			System.out.println(year + "is not a Leap year.");
+		}
+public static boolean isLeapyear(int year) {
+
+	return (year % 4 == 0 && year % 100 !=0)
+			|| year % 400 == 0;
+}
+}
